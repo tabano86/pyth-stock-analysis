a53de3c9384dc0867a0e6d020e8d8d644f72531e
Anthony Tabano <tabanoa@autonation.com>
2019-04-01 22:17:47 -0400

    added real data comparison

--- src/utils/ARIMAForecast.py
+++ src/utils/ARIMAForecast.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pandas.plotting import register_matplotlib_converters
@@ -23,12 +22,11 @@ def get_inverse_difference(history, yhat, interval=1):
     return yhat + history[-interval]
 
 
-def forecast(ticker: str):
+def forecast(ticker: str, as_of_date: np.datetime64 = None):
     DAYS_IN_YEAR = int(365)
+    FORECAST_DAYS = int(60)
 
     raw_dataset = get_json_data(ticker, TimePeriod.FIVE_YEARS)
-    FORECAST_DAYS = int(60)
-    TRAINING_DAYS = len(raw_dataset)
 
     raw_dataset['date'] = raw_dataset.index
 
@@ -36,16 +34,21 @@ def forecast(ticker: str):
     dataset.set_index('date', inplace=True)
     dataset = dataset.resample('D').ffill().reset_index()
     dataset.set_index('date', inplace=True)
-    split_date = np.datetime64('today') - np.timedelta64(TRAINING_DAYS, 'D')
+
+    if as_of_date is None:
+        split_date = dataset.index.max()
+    else:
+        split_date = as_of_date
+
     dataset['diff'] = dataset['close'].diff(DAYS_IN_YEAR)
-    # dataset = dataset[DAYS_IN_YEAR:]
+
     train = dataset[(dataset.index < split_date)]
-    valid = dataset[(dataset.index >= split_date)]
+    train = train[DAYS_IN_YEAR:]
+
     model = auto_arima(y=train['diff'].values,
                        start_p=1,
                        start_q=1,
                        test='adf',
-                       seasonal_test='seas',
                        max_p=5,
                        max_q=5,
                        m=7,
@@ -64,41 +67,20 @@ def forecast(ticker: str):
     output = pd.DataFrame(None, columns=['yhat', 'close', 'date'])
 
     for i in range(0, len(forecast)):
-        ref_date = split_date + np.timedelta64(i, 'D')
+        ref_date = split_date + np.timedelta64(i + 1, 'D')
         ref_yhat = forecast[i]
-        ref_close = ref_yhat + (dataset[(dataset.index == (ref_date - np.timedelta64(1, 'D')))])['close'][0]
+        ref_close = ref_yhat + train[(ref_date - np.timedelta64(DAYS_IN_YEAR, 'D') == train.index)]['close'].item()
         output = output.append({'yhat': ref_yhat, 'close': ref_close, 'date': ref_date}, ignore_index=True)
     output.set_index('date', inplace=True)
 
     output['movingAverage'] = output['close'].rolling(window=20, center=False,
-                                                      min_periods=1).mean()
+                                                      min_periods=0).mean()
     output['std'] = output['close'].rolling(window=20, center=False,
-                                            min_periods=1).std()
+                                            min_periods=0).std()
     output['upperBand'] = output['movingAverage'] + (output['std'] * 2)
     output['lowerBand'] = output['movingAverage'] - (output['std'] * 2)
 
     train['training close'] = train['close']
-    valid['valid close'] = valid['close']
     # output = pd.merge(output, train['training close'], how='left', left_index=True, right_index=True)
-    # output = pd.merge(output, valid['valid close'], how='right', left_index=True, right_index=True)
 
-    plot_output = output[(output.index >= np.datetime64('today') - np.timedelta64(TRAINING_DAYS, 'D'))]
-
-    plt.fill_between(x=plot_output.index,
-                     y1=plot_output['upperBand'].values,
-                     y2=plot_output['lowerBand'].values,
-                     color='grey', alpha=0.7,
-                     interpolate=True)
-
-    plt.plot(plot_output['lowerBand'], color='blue', lw=2, alpha=0.7)
-    plt.plot(plot_output['upperBand'], color='blue', lw=2, alpha=0.7)
-    # plt.plot(plot_output['training close'], label='Training Data')
-    # plt.plot(plot_output['valid close'], label='Actual Data')
-    plt.plot(plot_output['close'], label='Forecast Data')
-    plt.grid(axis='both', which='major')
-    plt.legend()
-    plt.autoscale()
-    plt.show()
-
-
-forecast('AMD')
+    return output[(output.index >= split_date - np.timedelta64(FORECAST_DAYS, 'D'))]
